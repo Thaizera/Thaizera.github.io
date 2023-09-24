3e1d7b36ecba00a69ee6056152e4b34a80c75891
Thaizera <145925048+Thaizera@users.noreply.github.com>
2023-09-24 14:27:10 -0300

    Add files via upload

---
+++ callapp.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import
+import random
+from tkinter import messagebox
+
+root = tk.Tk()
+root.title('aceitas?')
+root.geometry('600x600')
+root.configure(background='#ffcBdd')
+
+def move_button_1(e):
+      if abs(e.x - move_button_1.winfo_x()) 
